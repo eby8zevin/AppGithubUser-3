f64219cb2cf0c9c9ba693686e96381795cd70170
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-11-23 19:56:59 +0700

    Retrofit ApiConfig.java

---
+++ app/src/main/java/com/ahmadabuhasan/appgithubuser/api/ApiConfig.java
@@ -0,0 +1,45 @@
+package com.ahmadabuhasan.appgithubuser.api;
+
+import com.ahmadabuhasan.appgithubuser.BuildConfig;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiConfig {
+    public static class tokenInterceptor implements Interceptor {
+
+        @NotNull
+        @Override
+        public Response intercept(@NotNull Chain chain) throws IOException {
+            Request request = chain.request().newBuilder()
+                    .header("Authorization", "Bearer " + BuildConfig.TOKEN)
+                    .build();
+            return chain.proceed(request);
+        }
+    }
+
+    public static ApiService getApiService() {
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor()
+                .setLevel(HttpLoggingInterceptor.Level.BODY);
+        tokenInterceptor interceptor = new tokenInterceptor();
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .addInterceptor(interceptor)
+                .build();
+        Retrofit retrofit = new retrofit2.Retrofit.Builder()
+                .baseUrl("https://api.github.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+        return retrofit.create(ApiService.class);
+    }
+}
