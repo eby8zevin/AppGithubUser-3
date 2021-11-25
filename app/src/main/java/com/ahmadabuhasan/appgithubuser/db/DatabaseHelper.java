f006296db5a3acff29a515ba7f6cb83e6aa27581
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-11-25 23:35:32 +0700

    SQLite DatabaseHelper.java

---
+++ app/src/main/java/com/ahmadabuhasan/appgithubuser/db/DatabaseHelper.java
@@ -0,0 +1,41 @@
+package com.ahmadabuhasan.appgithubuser.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static com.ahmadabuhasan.appgithubuser.db.DatabaseContract.FavoriteColumns.TABLE_NAME;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "favorite.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String CREATE_TABLE = String.format(
+            "CREATE TABLE %s" +
+                    " (%s INTEGER PRIMARY KEY," +
+                    " %s TEXT NOT NULL," +
+                    " %s TEXT NOT NULL," +
+                    " %s TEXT NOT NULL)",
+            TABLE_NAME,
+            DatabaseContract.FavoriteColumns.ID,
+            DatabaseContract.FavoriteColumns.AVATAR,
+            DatabaseContract.FavoriteColumns.HTML,
+            DatabaseContract.FavoriteColumns.USERNAME
+            );
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+}
