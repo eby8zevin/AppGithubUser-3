4c0845a53e718531d14ec7eb0de151e15dac65e6
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-11-29 19:35:47 +0700

    UI SettingActivity.java

--- app/src/main/java/com/ahmadabuhasan/appgithubuser/ui/SettingActivity.java
+++ app/src/main/java/com/ahmadabuhasan/appgithubuser/ui/SettingActivity.java
@@ -4,9 +4,16 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.datastore.preferences.core.Preferences;
+import androidx.datastore.preferences.rxjava3.RxPreferenceDataStoreBuilder;
+import androidx.datastore.rxjava3.RxDataStore;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.ahmadabuhasan.appgithubuser.R;
 import com.ahmadabuhasan.appgithubuser.databinding.ActivitySettingBinding;
+import com.ahmadabuhasan.appgithubuser.setting.SettingPreferences;
+import com.ahmadabuhasan.appgithubuser.setting.SettingViewModelFactory;
+import com.ahmadabuhasan.appgithubuser.viewmodel.SettingViewModel;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -20,19 +27,25 @@ public class SettingActivity extends AppCompatActivity {
         binding = ActivitySettingBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        switchTheme();
+        showViewModel();
     }
 
-    private void switchTheme() {
-        binding.switchTheme.setOnCheckedChangeListener(((buttonView, isChecked) -> {
-            if (isChecked) {
+    private void showViewModel() {
+        RxDataStore<Preferences> dataStore = new RxPreferenceDataStoreBuilder(this, "settings").build();
+        SettingPreferences pref = SettingPreferences.getInstance(dataStore);
+        SettingViewModel settingViewModel = new ViewModelProvider(this, new SettingViewModelFactory(pref)).get(SettingViewModel.class);
+        settingViewModel.getThemeSettings().observe(this, isDarkModeActive -> {
+            if (isDarkModeActive) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
                 binding.switchTheme.setChecked(true);
-                binding.switchTheme.setText(getResources().getString(R.string.light_mode));
             } else {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                 binding.switchTheme.setChecked(false);
             }
-        }));
+        });
+
+        binding.switchTheme.setOnCheckedChangeListener((buttonView, isChecked) ->
+                settingViewModel.saveThemeSetting(isChecked)
+        );
     }
 }
