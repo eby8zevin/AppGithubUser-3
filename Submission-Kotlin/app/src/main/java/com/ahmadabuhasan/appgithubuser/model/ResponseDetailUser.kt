b6eece0c5a4d27f3aa15874b24f3599be5770f68
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-12-20 12:11:38 +0700

    Add value in parameters

--- Submission-Kotlin/app/src/main/java/com/ahmadabuhasan/appgithubuser/model/ResponseDetailUser.kt
+++ Submission-Kotlin/app/src/main/java/com/ahmadabuhasan/appgithubuser/model/ResponseDetailUser.kt
@@ -2,101 +2,35 @@ package com.ahmadabuhasan.appgithubuser.model
 
 import com.google.gson.annotations.SerializedName
 
-data class ResponseDetailUser(
+class ResponseDetailUser(
 
     @field:SerializedName("login")
-    val login: String,
+    var login: String? = null,
 
     @field:SerializedName("id")
-    val id: Int,
-
-    @field:SerializedName("node_id")
-    val nodeId: String,
+    var id: Int? = null,
 
     @field:SerializedName("avatar_url")
-    val avatarUrl: String,
-
-    @field:SerializedName("gravatar_id")
-    val gravatarId: String,
-
-    @field:SerializedName("url")
-    val url: String,
+    var avatarUrl: String? = null,
 
     @field:SerializedName("html_url")
-    val htmlUrl: String,
-
-    @field:SerializedName("followers_url")
-    val followersUrl: String,
-
-    @field:SerializedName("following_url")
-    val followingUrl: String,
-
-    @field:SerializedName("gists_url")
-    val gistsUrl: String,
-
-    @field:SerializedName("starred_url")
-    val starredUrl: String,
-
-    @field:SerializedName("subscriptions_url")
-    val subscriptionsUrl: String,
-
-    @field:SerializedName("organizations_url")
-    val organizationsUrl: String,
-
-    @field:SerializedName("repos_url")
-    val reposUrl: String,
-
-    @field:SerializedName("events_url")
-    val eventsUrl: String,
-
-    @field:SerializedName("received_events_url")
-    val receivedEventsUrl: String,
-
-    @field:SerializedName("type")
-    val type: String,
-
-    @field:SerializedName("site_admin")
-    val siteAdmin: Boolean,
+    var htmlUrl: String? = null,
 
     @field:SerializedName("name")
-    val name: String,
+    val name: String? = null,
 
     @field:SerializedName("company")
-    val company: String,
-
-    @field:SerializedName("blog")
-    val blog: String,
+    val company: String? = null,
 
     @field:SerializedName("location")
-    val location: String,
-
-    @field:SerializedName("email")
-    val email: String,
-
-    @field:SerializedName("hireable")
-    val hireable: String,
-
-    @field:SerializedName("bio")
-    val bio: String,
-
-    @field:SerializedName("twitter_username")
-    val twitterUsername: String,
+    val location: String? = null,
 
     @field:SerializedName("public_repos")
-    val publicRepos: String,
-
-    @field:SerializedName("public_gists")
-    val publicGists: String,
+    val publicRepos: String? = null,
 
     @field:SerializedName("followers")
-    val followers: String,
+    val followers: String? = null,
 
     @field:SerializedName("following")
-    val following: String,
-
-    @field:SerializedName("created_at")
-    val createdAt: String,
-
-    @field:SerializedName("updated_at")
-    val updatedAt: String
+    val following: String? = null
 )
