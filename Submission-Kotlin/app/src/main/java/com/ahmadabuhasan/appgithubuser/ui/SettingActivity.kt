efefa1c3a4c475ef0700b8c77d242230092bd3c6
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-12-20 12:17:05 +0700

    Business Logic Dark-Mode

---
+++ Submission-Kotlin/app/src/main/java/com/ahmadabuhasan/appgithubuser/ui/SettingActivity.kt
@@ -0,0 +1,51 @@
+package com.ahmadabuhasan.appgithubuser.ui
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
+import com.ahmadabuhasan.appgithubuser.databinding.ActivitySettingBinding
+import com.ahmadabuhasan.appgithubuser.setting.SettingPreferences
+import com.ahmadabuhasan.appgithubuser.setting.ViewModelFactory
+import com.ahmadabuhasan.appgithubuser.viewmodel.SettingViewModel
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore("settings")
+
+class SettingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySettingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val switchTheme = binding.switchTheme
+
+        val pref = SettingPreferences.getInstance(dataStore)
+        val viewModel = ViewModelProvider(
+            this,
+            ViewModelFactory(pref)
+        )[SettingViewModel::class.java]
+        viewModel.getThemeSettings().observe(
+            this
+        ) { isDarkModeActive: Boolean ->
+            if (isDarkModeActive) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                switchTheme.isChecked = true
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                switchTheme.isChecked = false
+            }
+        }
+
+        switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
+            viewModel.saveThemeSetting(isChecked)
+        }
+    }
+}
